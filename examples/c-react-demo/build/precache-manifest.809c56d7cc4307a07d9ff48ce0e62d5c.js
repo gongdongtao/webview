3b4986dee83bb1f38fc62395d953af10e38381c5
kai.yan <kai.yan@dragonflow.com>
2019-10-12 19:27:50 +0800

    add build dir

---
+++ examples/c-react-demo/build/precache-manifest.809c56d7cc4307a07d9ff48ce0e62d5c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4339607f741baf0cb87",
+    "url": "/static/css/4.3d0f77d5.chunk.css"
+  },
+  {
+    "revision": "e8ee525a68ad308547e5",
+    "url": "/static/css/0.28917a76.chunk.css"
+  },
+  {
+    "revision": "9e44167e9810e87cf76e",
+    "url": "/static/css/main.af9dc4cc.chunk.css"
+  },
+  {
+    "revision": "9e44167e9810e87cf76e",
+    "url": "/static/js/main.9e44167e.chunk.js"
+  },
+  {
+    "revision": "1246e62718d4ae5cc632",
+    "url": "/static/js/runtime~main.1246e627.js"
+  },
+  {
+    "revision": "f795b8ca8a81d721083a",
+    "url": "/static/css/3.65de607d.chunk.css"
+  },
+  {
+    "revision": "f795b8ca8a81d721083a",
+    "url": "/static/js/3.f795b8ca.chunk.js"
+  },
+  {
+    "revision": "e8ee525a68ad308547e5",
+    "url": "/static/js/0.e8ee525a.chunk.js"
+  },
+  {
+    "revision": "b4339607f741baf0cb87",
+    "url": "/static/js/4.b4339607.chunk.js"
+  },
+  {
+    "revision": "de3153d20f847ed9786e",
+    "url": "/static/js/5.de3153d2.chunk.js"
+  },
+  {
+    "revision": "a8174c6adb8ec15b4893",
+    "url": "/static/js/6.a8174c6a.chunk.js"
+  },
+  {
+    "revision": "df6364a47724053a541b",
+    "url": "/static/js/7.df6364a4.chunk.js"
+  },
+  {
+    "revision": "140c0d54ccc4e2acf07991e07903af1c",
+    "url": "/static/media/network-4430786_1920.140c0d54.jpg"
+  },
+  {
+    "revision": "b11025588825d84615e100a8c97d63f8",
+    "url": "/static/media/Logo_VLAN_White.b1102558.png"
+  },
+  {
+    "revision": "772df8bfe835a3e62e344134f662230e",
+    "url": "/index.html"
+  }
+];
